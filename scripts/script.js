bea33b1768ba91fc9a4d2076ddbfcbd55da1e248
Timothy Chai <s10256132@connect.np.edu.sg>
2024-02-03 20:28:12 +0800

    Add lottie animations for some trivia topics

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,16 @@
+document.addEventListener("DOMContentLoaded", function () {
+    document.body.style.opacity = 1;
+});
+
+let triviaTopic = "general-knowledge";
+const topicRadio = document.getElementById("topic-radio");
+
+topicRadio.addEventListener("change", updateTopic);
+
+function updateTopic(event) {
+    console.log(event.target.value);
+}
+
 document.getElementById("start-trivia-button").onclick = function () {
     location.href = "trivia.html";
 };
